ec818bc1a26c903a8d2ac18fb95e00cc9098f9c4
Aaron Lin <aaronlin0516@hotmail.com>
2019-03-21 14:35:23 +0800

    二維商品建立練習

---
+++ products.py
@@ -0,0 +1,15 @@
+#記帳程式專案(+二維清單)
+products = []
+while True:
+	name = input('請輸入商品名稱: ')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格: ')
+	#p = []
+	#p.append(name)
+	#p.append(price)
+	#p = [name, price] #等同上方三行程式
+	products.append([name,price]) #等同上方四行程式
+
+print(products)
+
