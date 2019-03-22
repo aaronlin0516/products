66b35d5aebe60045ba27cb8a78e82740d4935454
Aaron Lin <aaronlin0516@hotmail.com>
2019-03-22 14:58:29 +0800

    讀取檔案

--- products.py
+++ products.py
@@ -1,5 +1,14 @@
 #記帳程式專案(+二維清單)
+#讀取檔案
 products = []
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue							#如果符合"商品,價格"就跳到下一迴，以下條件跳過，繼續下一迴處理
+		name, price = line.strip().split(',')	#先把多餘的符號清除(strip)再用','切割(split)
+		products.append([name,price])
+print(products)
+
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
@@ -10,7 +19,6 @@ while True:
 	#p.append(price)
 	#p = [name, price] #等同上方三行程式
 	products.append([name,price]) #等同上方四行程式
-
 print(products) #印出大清單
 
 for p in products:   #印出小清單
