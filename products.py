984be39cc64aefbcb68fc52bbb5bdb4ef2409a9e
Aaron Lin <aaronlin0516@hotmail.com>
2019-03-22 13:55:09 +0800

    寫入csv檔及語系問題

--- products.py
+++ products.py
@@ -11,7 +11,12 @@ while True:
 	#p = [name, price] #等同上方三行程式
 	products.append([name,price]) #等同上方四行程式
 
-print(products)
+print(products) #印出大清單
 
-for p in products:
-	print(p)
+for p in products:   #印出小清單
+	print(p[0], '的價格是', p[1])
+
+with open('products.csv', 'w', encoding = 'utf-8') as f:		#打開檔案寫入products.csv ","可以做分格區分
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n') 	#用+合併必須為字串
